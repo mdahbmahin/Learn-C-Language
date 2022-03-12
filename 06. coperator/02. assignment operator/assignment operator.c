3049c979bbbc2e455cb63495fa092ca4269a0abf
Md. Ahb Mahin <89274582+mdahbmahin@users.noreply.github.com>
2022-03-12 04:53:03 -0800

    Add files via upload

---
+++ 06. coperator/02. assignment operator/assignment operator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    a += 5; // a = a+5
+    printf("%d\n",a);
+
+    b -= 5; // b = b-5
+    printf("%d\n",b);
+
+    c *= 5; // c = c+5
+    printf("%d\n",c);
+
+    getch();
+}
