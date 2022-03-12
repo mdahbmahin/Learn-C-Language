23f9c0d353a20f705c9a99490c742425c43485f0
Md. Ahb Mahin <89274582+mdahbmahin@users.noreply.github.com>
2022-03-12 04:42:53 -0800

    Add files via upload

---
+++ 02. comment and escape sequence/comment and escape sequence.c
@@ -0,0 +1,43 @@
+// hello this program created to show comment and escape sequence in program
+#include<stdio.h>
+int main()
+{
+    // this is my single line comment , next i'll create the double line comment
+    /* this is my double line comment
+     and then i'll create escape sequence*/
+
+     /* c programs has tokens,now we will see the token list with example
+      keyword = int, float,return etc
+      constance = 100,-500,PI etc
+      identifire = sum main etc
+      strings = "hello", "hi" etc
+      operators = +,-,*,/ etc
+      special simbols = first bracket, second brackets etc
+
+     */
+
+     /* c programs escape sequence list
+       \a = alert or bell
+       \n = new line
+       \f = form feed
+       \r = carrige return
+       \t = horizontal tab
+       \v = vertical tab
+       \0 = null character
+       \' = single quote
+       \" = double quote
+       \\ = backslash
+       \? = question mark
+       our all escape sequence are here
+       now we wiil use it  */
+
+    printf("Mahin\n");
+    printf("mahi\bn\n");
+    printf("Mahin\t");
+    printf("\'Mahin\'\n");
+    printf("\"Mahin\"\n");
+
+
+    return 0;// also we can use getch function to hide compile details.
+
+}
