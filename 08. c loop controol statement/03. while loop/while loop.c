3d3af1b76d3ee24efbbbf1880aa156c86a7be528
Md. Ahb Mahin <89274582+mdahbmahin@users.noreply.github.com>
2022-03-12 05:09:24 -0800

    Add files via upload

---
+++ 08. c loop controol statement/03. while loop/while loop.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=5)// while( condition check){ }
+    {
+        printf("%d",i);
+        printf(". c program\n");
+        i++;
+
+    }
+    return 0;
+}
