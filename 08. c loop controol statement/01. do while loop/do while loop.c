8117845aea177c78844b02791f0081454f2092e7
Md. Ahb Mahin <89274582+mdahbmahin@users.noreply.github.com>
2022-03-12 05:07:40 -0800

    Add files via upload

---
+++ 08. c loop controol statement/01. do while loop/do while loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;// initialization
+
+    do
+    {
+        printf("%d",i);
+        printf(". c program\n");
+        i++; // increment part
+    }
+    while(i<=5);// condition check
+
+
+    return 0;
+}
+
