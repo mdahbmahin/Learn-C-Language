09d04553cd3b2d39bb336032e6c059eac5c6efbd
Md. Ahb Mahin <89274582+mdahbmahin@users.noreply.github.com>
2022-03-12 05:08:31 -0800

    Add files via upload

---
+++ 08. c loop controol statement/02. for loop/for loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1; i<=5; i++)// for( initialization ; condition check; counter updater){ }
+    {
+        printf("%d",i);
+        printf(". c program\n");
+    }
+
+    printf("----------------------\n");
+
+    int t;
+    for(t=0; t<=100; t=t+2)// for( initialization ; condition check; counter updater){ }
+    {
+        printf("%d\n",t);
+    }
+
+
+    return 0;
+}
