479f7c9972f66aed33f429b7b60149874ed683c5
Md. Ahb Mahin <89274582+mdahbmahin@users.noreply.github.com>
2022-03-12 10:43:18 -0800

    Add files via upload

---
+++ 10. storage class/01. storage class auto/storage class auto one.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void up();
+int main()
+{
+        up();
+        up();
+        up();
+        up();
+}
+
+void up()
+{
+    auto int i=12;
+    printf("%d\n",i);
+    i++;
+
+}
