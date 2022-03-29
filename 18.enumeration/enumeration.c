194ce79396fc43a2dbac5762c38be6152bd10182
Md. Ahb Mahin <89274582+mdahbmahin@users.noreply.github.com>
2022-03-29 11:41:19 +0600

    Add files via upload

---
+++ 18.enumeration/enumeration.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+enum daysofweek
+{
+    sunday,monday,tuesday,wednesday,thursday,friday,saturday
+};
+int main()
+{
+    enum daysofweek day1,day2;
+    day1= tuesday;
+    printf("day1 = %d\n",day1);
+
+
+    getch();
+}
